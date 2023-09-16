bbff062c0701c3d90ac0121d21c6437d9ef75260
Mig Tam Rod <migtam628@gmail.com>
2023-09-15 22:58:06 -0400

    dist update

--- dist/assets/ios.transition-legacy-0d1395c5.js
+++ dist/assets/ios.transition-legacy-0d1395c5.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-47a401ef.js"],(function(e,t){"use strict";var o,n;return{setters:[e=>{o=e.l,n=e.m}],execute:function(){
+System.register(["./index-legacy-308a037c.js"],(function(e,t){"use strict";var o,n;return{setters:[e=>{o=e.l,n=e.m}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
